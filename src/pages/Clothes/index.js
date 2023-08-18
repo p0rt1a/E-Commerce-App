1e4c0be9240c4c60d1657de20814c8c2ebd01f9c
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-08-18 17:58:44 +0300

    create: Clothes page

---
+++ src/pages/Clothes/index.js
@@ -0,0 +1,32 @@
+import { getClothes } from "../../clothesApi";
+import { useQuery } from "react-query";
+import ClothCard from "../../components/ClothCard";
+
+import { Flex, Box, Container, VStack } from "@chakra-ui/react";
+
+function Clothes() {
+  const { isLoading, error, data } = useQuery("clothes", getClothes);
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error occured: " + error.message;
+
+  return (
+    <div>
+      Clothes works!
+      <Container maxW={"container.xl"}>
+        <Flex wrap={"wrap"} gap={"10"}>
+          {data.map((item, i) => {
+            return (
+              <Box key={i}>
+                <ClothCard item={item} />
+              </Box>
+            );
+          })}
+        </Flex>
+      </Container>
+    </div>
+  );
+}
+
+export default Clothes;

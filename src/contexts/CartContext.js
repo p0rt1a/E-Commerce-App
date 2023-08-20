94ac62976f9f8d7a1623cb1919403ab0a0e7ee74
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-08-20 18:12:45 +0300

    add: Cart context

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -5,9 +5,16 @@ const CartContext = createContext();
 const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
 
+  const removeItem = (deletedItem) => {
+    var filtered = cartItems.filter((item) => item.id !== deletedItem.id);
+
+    setCartItems(filtered);
+  };
+
   const values = {
     cartItems,
     setCartItems,
+    removeItem,
   };
 
   return <CartContext.Provider value={values}>{children}</CartContext.Provider>;

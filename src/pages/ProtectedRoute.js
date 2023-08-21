a5463a913b048ee85a50bf6df3c2d9f2ea665731
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-08-21 21:01:56 +0300

    update: Redirect operations for admin panel

--- src/pages/ProtectedRoute.js
+++ src/pages/ProtectedRoute.js
@@ -1,13 +1,17 @@
 import { Route, Redirect } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
-function ProtectedRoute({ component: Component, ...rest }) {
-  const { loggedIn } = useAuth();
+function ProtectedRoute({ component: Component, admin, ...rest }) {
+  const { loggedIn, user } = useAuth();
 
   return (
     <Route
       {...rest}
       render={(props) => {
+        if (admin && user != null && user.role != "admin") {
+          return <Redirect to={{ pathname: "/" }} />;
+        }
+
         if (loggedIn) {
           return <Component {...props} />;
         }

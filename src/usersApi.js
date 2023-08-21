a7ab0e7b52241a3bcfc2cd55588de648eb6b985a
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-08-21 21:00:43 +0300

    add: getUsers method

--- src/usersApi.js
+++ src/usersApi.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+export const getUsers = async () => {
+  const { data } = await axios.get(
+    `${process.env.REACT_APP_BASE_ENDPOINT}/users`
+  );
+
+  return data;
+};
+
 export const getUserDetails = async (id) => {
   const { data } = await axios.get(
     `${process.env.REACT_APP_BASE_ENDPOINT}/users/${id}`

967b1e2fbf613e028b0082e58d256eb35012fc6f
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-08-21 21:00:17 +0300

    add: createcloth and deletecloth methods

--- src/clothesApi.js
+++ src/clothesApi.js
@@ -15,3 +15,20 @@ export const getClothDetails = async (id) => {
 
   return data;
 };
+
+export const createCloth = async (item) => {
+  const { response } = await axios.post(
+    `${process.env.REACT_APP_BASE_ENDPOINT}/clothes`,
+    item
+  );
+
+  return response;
+};
+
+export const deleteCloth = async (id) => {
+  const response = await axios.delete(
+    `${process.env.REACT_APP_BASE_ENDPOINT}/clothes/${id}`
+  );
+
+  return response;
+};

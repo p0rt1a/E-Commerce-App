a73013b5dbd1109f135e73dde95e78a5ff82a0b3
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-09-14 14:39:38 +0300

    bug fix: login bug fix, checks password match now!

--- src/authApi.js
+++ src/authApi.js
@@ -2,8 +2,10 @@ import axios from "axios";
 
 export const LoginAccount = async (email, password) => {
   const { data } = await axios.get(
-    `${process.env.REACT_APP_BASE_ENDPOINT}/users?email=${email}&password=${password}`
+    `${process.env.REACT_APP_BASE_ENDPOINT}/users?email=${email}`
   );
+
+  if (data[0].password !== password) throw new Error("Incorrect password!");
 
   return data;
 };
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -14,6 +14,7 @@ const AuthProvider = ({ children }) => {
 
   const logout = () => {
     setLoggedIn(false);
+    localStorage.removeItem("user_id");
     setUser(null);
   };
 
